f2a4c1ae730894f5336996cd8196967ccc22e241
Username <abc@gmail.com>
2019-05-10 18:32:04 +0530

    Grid_demo First Commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import React from 'react';
+function App() {
+  return (
+    <div className="parentEle">
+     <div className="gridContainer">
+      <div className="itemA">A</div>
+      <div className="itemB">B</div>
+      <div className="itemC">C</div>
+      <div className="itemD">D</div>
+      <div className="itemE">E</div>
+      <div className="itemF">F</div>
+      <div className="itemG">G</div>
+      <div className="itemH">H</div>
+      <div className="itemI">I</div>
+      <div className="itemJ">J</div>
+      <div className="itemK">K</div>
+      <div className="itemL">L</div>
+      <div className="itemM">M</div>
+     </div>
+    </div>
+  );
+}
+
+export default App;
